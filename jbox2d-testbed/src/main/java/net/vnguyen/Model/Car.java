e857e3468c254ab215e3ab81fedf0ca8f76707cc
nguyenmv2 <nguyenmv2@hendrix.edu>
2015-12-10 07:21:02 -0600

    Working Car

--- jbox2d-testbed/src/main/java/net/vnguyen/Model/Car.java
+++ jbox2d-testbed/src/main/java/net/vnguyen/Model/Car.java
@@ -1,15 +1,19 @@
 package net.vnguyen.Model;
 
+import com.sun.org.apache.xpath.internal.SourceTree;
 import org.jbox2d.collision.shapes.CircleShape;
 import org.jbox2d.common.Vec2;
 import org.jbox2d.dynamics.*;
 import org.jbox2d.dynamics.joints.*;
 
+import javax.sound.midi.Soundbank;
+
 /**
  * Created by Test on 12/9/2015.
  */
 public class Car {
-    
+    private static final float MAX_HEALTH = 200;
+
     private Body chassis;
     public CarShape carShape;
     public Body[] wheels;
@@ -17,7 +21,20 @@ public class Car {
     public float[] torque;
     public World world;
 
-    public Car(CarShape carShape){
+    public float maxX = 0, maxY = 0, maxPos= 0 ;
+    public float health = MAX_HEALTH;
+    public int stepCount = 0;
+    public boolean isAlive = true;
+    public float score;
+
+    public Car(CarShape carShape) {
+
+        generateCar(carShape);
+    }
+
+
+
+    public void generateCar(CarShape carShape) {
         this.carShape = carShape;
         this.world = carShape.world;
         this.chassis = carShape.createChassis(carShape.vertexList, carShape.chassisDensity);
@@ -25,8 +42,8 @@ public class Car {
         this.torque = new float[carShape.wheelCount];
         initCar();
     }
-    
-    public void initCar(){
+
+    public void initCar() {
         /*
         ** Making wheels
          */
@@ -37,49 +54,42 @@ public class Car {
         calculateCarMass();
         calculateTorque();
         createJoint();
-        
+
     }
-    public void calculateCarMass(){
+
+    public void calculateCarMass() {
         carMass = chassis.getMass();
         for (int i = 0; i < carShape.wheelCount; i++) {
             carMass += wheels[i].getMass();
         }
-        System.out.println(carMass);
     }
-    public void calculateTorque(){
+
+    public void calculateTorque() {
         for (int i = 0; i < carShape.wheelCount; i++) {
-            torque[i] = carMass * (- world.getGravity().y) / carShape.wheelRadius[i];
+            torque[i] = carMass * (-world.getGravity().y) / carShape.wheelRadius[i];
         }
     }
-    public void createJoint(){
+
+    public void createJoint() {
         RevoluteJointDef jointDef = new RevoluteJointDef();
         for (int i = 0; i < carShape.wheelCount; i++) {
-
             jointDef.bodyA = this.chassis;
             jointDef.bodyB = this.wheels[i];
             jointDef.collideConnected = false;
             Vec2 wheelPlacement = carShape.vertexList[carShape.wheelVertex[i]];
-            jointDef.localAnchorA.set((wheelPlacement).x,(wheelPlacement).y);
-            jointDef.localAnchorB.set(0,0);
-//            jointDef.initialize(this.chassis, this.wheels[i], this.wheels[i].getPosition());
+            jointDef.localAnchorA.set((wheelPlacement).x, (wheelPlacement).y);
             jointDef.maxMotorTorque = torque[i];
-            jointDef.motorSpeed = - carShape.MotorSpeed;
+            jointDef.motorSpeed = -carShape.MotorSpeed;
             jointDef.enableMotor = true;
-
-
-            RevoluteJoint joint =(RevoluteJoint) world.createJoint(jointDef);
-
-//            joint.enableMotor(true);
-//            joint.enableLimit(false);
-
-
+            RevoluteJoint joint = (RevoluteJoint) world.createJoint(jointDef);
         }
     }
 
-    public Vec2 getPosition(){
+    public Vec2 getPosition() {
         return this.chassis.getPosition();
     }
-    public Body createWheel(float radius, float density,Vec2 axelPos){
+
+    public Body createWheel(float radius, float density, Vec2 axelPos) {
         BodyDef bodyDef = new BodyDef();
         bodyDef.type = BodyType.DYNAMIC;
         bodyDef.position.set(axelPos);
@@ -95,4 +105,50 @@ public class Car {
         body.createFixture(fixDef);
         return body;
     }
+
+    public boolean isDead() {
+
+        Vec2 curPos = this.getPosition();
+        if (curPos.y > maxY){
+            maxY = curPos.y;
+        }
+        if (curPos.x > maxPos +0.02f){
+            this.health = MAX_HEALTH;
+            this.maxPos = curPos.x;
+        } else {
+            if (curPos.x > this.maxPos){
+                maxPos = curPos.x;
+            }
+//            if (Math.abs(this.chassis.getLinearVelocity().x) < 0.0001){
+//                this.health -= 5;
+//            }
+            this.health --;
+            if (this.health <= 0){
+
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public float kill(){
+
+        float speed =(float) (this.maxPos / this.stepCount) * 1/60;
+        float pos = maxPos;
+        this.score = pos + speed;
+        world.destroyBody(this.chassis);
+        for (int i = 0; i < this.wheels.length; i++) {
+            world.destroyBody(wheels[i]);
+        }
+        this.isAlive = false;
+        return score;
+    }
+
+    public boolean equals(Car other){
+        if (this.carMass == other.carMass){
+            return true;
+        }
+        return false;
+    }
+
 }
